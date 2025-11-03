eb6e76fc0760c9703ecfa2c24d2f195cd3e4f275
Gargi Porwal <gargi.porwal.2828@gmail.com>
2025-11-03 13:17:52 +0530

    Note App

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
+
 const {
   registerUser,
   loginUser,
   getMe,
 } = require("../controllers/userController");
+
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
